a3ab2a23b32d755ebfec4cf655d6fc55e6525eb6
FedericoMuccioli <federicomuccioliabc@gmail.com>
2023-02-22 17:50:24 +0100

    Implement main

--- room-controller/src/main.cpp
+++ room-controller/src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
+#include "config.h"
+#include "kernel/MsgService.h"
+#include "kernel/Scheduler.h"
+#include "task/SmartLightingTask.h"
 
-void setup() {
-  // put your setup code here, to run once:
+Scheduler sched;
+
+void setup() 
+{
+  sched.init(SCHEDULER_PERIOD);
+  MsgService.init(BOUND);
+  Task* smartLightingTask = new SmartLightingTask();
+  smartLightingTask->init(SMART_LIGHTING_PERIOD);
+  sched.addTask(smartLightingTask);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  sched.schedule();
 }
