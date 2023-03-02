a729402596ef9765a4a14067a283bb4ae7ace040
Nicola Montanari <Nicola.Montanari14@studio.unibo.it>
2023-03-02 19:14:16 +0100

    Test JSON library

--- room-service/src/main/RoomService.java
+++ room-service/src/main/RoomService.java
@@ -4,6 +4,11 @@ import communication.RoomControllerComm;
 import communication.SerialCommChannel;
 import controller.Controller;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import Json.JsonManager;
+
 public class RoomService {
 	
 	/**
@@ -20,15 +25,26 @@ public class RoomService {
 	 * @throws Exception if port incorrect
 	 */
 	public static void main(String[] args) throws Exception {
-		final String port = args[0];
+		//final String port = args[0];
 		
-		CommChannel comm = new SerialCommChannel(port, BAUD);
-		System.out.println("Waiting Arduino for rebooting...");
-		Thread.sleep(5000);
+		//CommChannel comm = new SerialCommChannel(port, BAUD);
+		//System.out.println("Waiting Arduino for rebooting...");
+		//Thread.sleep(5000);
 		System.out.println("Ready.");
 		
-		Controller controller = new Controller(new RoomControllerComm(comm));
-		controller.start();
+		//Controller controller = new Controller(new RoomControllerComm(comm));
+		//controller.start();
+		
+		JsonManager jm = new JsonManager();
+		
+		//jm.CreateNewJson("today", "10:30", "ON", "80");
+		
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String formattedDate = today.format(formatter);
+        
+        jm.CreateNewJson(formattedDate, "10:30", "ON", "80");
+		jm.AddRowToJSON(formattedDate, "10:45", "OFF", "52");
 	}
 
 }
