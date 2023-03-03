6f936530205a1dfed73125b0b6b802c1b05c4190
FedericoMuccioli <federicomuccioliabc@gmail.com>
2023-03-03 18:42:55 +0100

    Add in Server reception of Room state

--- room-service/src/communication/RoomControllerComm.java
+++ room-service/src/communication/RoomControllerComm.java
@@ -2,21 +2,55 @@ package communication;
 
 public class RoomControllerComm {
 	
-	private final static String msgLight = "l";
-	private final static String msgDegree = "d";
+	private final static String LIGHT = "l";
+	private final static String ROLLER_BLINDS = "d";
+	private final static String REGEX = "&";
 	
 	private final CommChannel comm;
+	private int light;
+	private int rollerBlinds;
 	
 	public RoomControllerComm(final CommChannel commChannel) {
 		this.comm = commChannel;
 	}
 	
+	public boolean isChangeState() {
+		String state = null;
+		while (comm.isMsgAvailable()) {
+			try {
+				state = comm.receiveMsg();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				return false;
+			}
+			var split = state.split(REGEX);
+			for (String s : split) {
+				String typeValue = Character.toString(s.charAt(0));
+				final int value = Integer.parseInt(s.substring(1));
+				if (typeValue.equals(LIGHT)) {
+					light = value;
+				} else if (typeValue.equals(ROLLER_BLINDS)) {
+					rollerBlinds = value;
+				}
+			}
+		}
+		return state != null;
+	}
+	
 	public void setLight(final boolean light) {
-		comm.sendMsg(msgLight + (light == true ? "1" : "0"));
+		comm.sendMsg(LIGHT + (light == true ? "1" : "0"));
 	}
 	
 	public void setRollerBlinds(final int degree) {
-		comm.sendMsg(msgDegree + String.valueOf(degree));
+		comm.sendMsg(ROLLER_BLINDS + String.valueOf(degree));
+	}
+	
+	public int getLight() {
+		return light;
+	}
+
+	public int getRollerBlinds() {
+		return rollerBlinds;
 	}
 
 }
