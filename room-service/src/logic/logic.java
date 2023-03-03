2d65df00e537984a5692b3c45706e02737fb5455
Nicola Montanari <Nicola.Montanari14@studio.unibo.it>
2023-03-03 22:04:07 +0100

    Add Controller

--- room-service/src/logic/logic.java
+++ room-service/src/logic/logic.java
@@ -8,11 +8,11 @@ import Json.JsonManager;
 public class logic {
 
     int LIGHT_TRESHOLD = 400;
-    // RoomControllerComm Controller;
+    RoomControllerComm Controller;
     JsonManager jm;
 
-    public logic(JsonManager jm){
-        // this.Controller = Controller;
+    public logic(RoomControllerComm Controller, JsonManager jm){
+        this.Controller = Controller;
         this.jm = jm;
     }
 
@@ -24,20 +24,20 @@ public class logic {
 
         if(motionValue=="ON"){
             if(luminosityValue<LIGHT_TRESHOLD){
-                // Controller.setLight(true);
+                Controller.setLight(true);
                 //jm.updateLights("ON");
             }
             if(oraCorrente>=8 && oraCorrente<=19){
                 //Persiane tutte su
-                // Controller.setRollerBlinds(0);
+                Controller.setRollerBlinds(0);
                 //jm.updateBlinds(0);
             }
         }else{
-            // Controller.setLight(false);
+            Controller.setLight(false);
             //jm.updateLights("OFF");
             if(oraCorrente>=19){
                 //Persiane tutte giù
-                // Controller.setRollerBlinds(180);
+                Controller.setRollerBlinds(180);
                 //jm.updateBlinds(100);
             }
         }
