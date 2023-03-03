4c4100856d3184676d27280f3a259dae8fd963ba
Nicola Montanari <Nicola.Montanari14@studio.unibo.it>
2023-03-03 23:07:19 +0100

    Add main functionalities to controller

--- room-service/src/controller/Controller.java
+++ room-service/src/controller/Controller.java
@@ -1,26 +1,33 @@
 package controller;
 
+import Json.JsonManager;
 import communication.RoomControllerComm;
+import logic.logic;
 
 public class Controller {
-	
-	private final RoomControllerComm roomContrComm;
 
-	public Controller(final RoomControllerComm roomContrComm) {
+	private final RoomControllerComm roomContrComm;
+	private final logic logic;
+	private final JsonManager jm;
+
+	public Controller(final RoomControllerComm roomContrComm, final logic logic, final JsonManager jm) {
 		this.roomContrComm = roomContrComm;
+		this.logic = logic;
+		this.jm = jm;
 	}
-	
-	public void start() throws InterruptedException { //cancellare throw
-		while(true) {
-			Thread.sleep(2000);
-			roomContrComm.setLight(true);
-			Thread.sleep(100);
-			roomContrComm.setRollerBlinds(180);
-			Thread.sleep(2000);
-			roomContrComm.setLight(false);
-			Thread.sleep(100);
-			roomContrComm.setRollerBlinds(0);
+
+	public void updateRoom(String movement, int luminosity) {
+		String[] values = logic.UpdateLogic(movement, luminosity);
+		if(values[0] != "null") {
+			roomContrComm.setLight(values[0] == "ON" ? true : false);
+		}
+		if(values[1] != "null") {
+			roomContrComm.setRollerBlinds(Integer.parseInt(values[1]));
 		}
 	}
-	
+
+	public void notifyChangeRoom(final int light, final int rollerBlinds) {
+        jm.UpdateJSON(light, rollerBlinds);
+    }
+
 }
