5d91837b0366a6fb5ed067b42b3bcb9b2eb9066b
Nicola Montanari <Nicola.Montanari14@studio.unibo.it>
2023-03-03 23:06:38 +0100

    Update logic, removed controller

--- room-service/src/logic/logic.java
+++ room-service/src/logic/logic.java
@@ -1,45 +1,37 @@
 package logic;
 
 import java.time.LocalTime;
-
-import communication.RoomControllerComm;
 import Json.JsonManager;
 
 public class logic {
 
     int LIGHT_TRESHOLD = 400;
-    RoomControllerComm Controller;
     JsonManager jm;
 
-    public logic(RoomControllerComm Controller, JsonManager jm){
-        this.Controller = Controller;
+    public logic(JsonManager jm){
         this.jm = jm;
     }
 
-    public void UpdateLogic(String msg){
-        String[] msgArray = msg.split("&");
-        String motionValue = msgArray[0].split(":")[1];
-        int luminosityValue = Integer.parseInt(msgArray[1].split(": ")[1]);
+    public String[] UpdateLogic(String motion, int luminosity){
         int oraCorrente = (LocalTime.now()).getHour();
-
-        if(motionValue=="ON"){
-            if(luminosityValue<LIGHT_TRESHOLD){
-                Controller.setLight(true);
-                //jm.updateLights("ON");
+        String[] ArrayValues = {"null", "null"};
+        if(motion=="ON"){
+            if(luminosity<LIGHT_TRESHOLD){
+                //Luci accese
+                ArrayValues[0] = "ON";
             }
             if(oraCorrente>=8 && oraCorrente<=19){
                 //Persiane tutte su
-                Controller.setRollerBlinds(0);
-                //jm.updateBlinds(0);
+                ArrayValues[1] = "0";
             }
         }else{
-            Controller.setLight(false);
-            //jm.updateLights("OFF");
+            //Luci spente
+            ArrayValues[0] = "OFF";
             if(oraCorrente>=19){
                 //Persiane tutte giù
-                Controller.setRollerBlinds(180);
-                //jm.updateBlinds(100);
+                ArrayValues[1] = "100";
             }
         }
+        return ArrayValues;
     }
 }
