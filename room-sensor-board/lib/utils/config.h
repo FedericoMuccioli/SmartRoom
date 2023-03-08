b26bc0bd433a7ba76c2471b1806ea254cbdabc34
Nicola Montanari <Nicola.Montanari14@studio.unibo.it>
2023-03-08 19:43:23 +0100

    Code Refactoring

--- room-sensor-board/lib/utils/config.h
+++ room-sensor-board/lib/utils/config.h
@@ -7,14 +7,11 @@ typedef enum{
 } State;
 
 #define BOUND 115200
-#define SCHEDULER_PERIOD 100
 
-#define WIFI_SSID "TIM-47108471"
-#define WIFI_PASSWORD "EZu3DG9eSqhd3dubP4CdN5dQ"
+#define WIFI_SSID "ssid"
+#define WIFI_PASSWORD "psw"
 #define MQTT_SERVER_ADDRESS "broker.hivemq.com"
 #define MQTT_TOPIC "ESP32-IoT-2023"
-
-#define MSG_BUFFER_SIZE  50
 
 #define LED_PIN 12
 #define PIR_PIN 11
